a8bc9eda4418e304e1c3a70ba5676fa1a4c6f8d7
Damian Boquete <damian.boquete@hotmail.com>
2024-11-29 17:12:50 +0100

    Updated code behaviour

--- tests_ressources/main.py
+++ tests_ressources/main.py
@@ -10,18 +10,17 @@ import numpy as np
 import math
 import os
 
-# TODO: clean path handling
-
-# TEMPPATH = "temp"
-# LOGPATH = "logs"
-# INPUTPATH = "input"
-# OUTPUTPATH = "output"
-
 
 def write_patient() -> Patient:
-    trial_data = pd.read_excel("input/PRE-ACT-01_Flow2_20241115_HES-SO.xlsx", index_col=False).iloc[:, :5]
-    clinical_data = dh.obtain_data("input/PRE-ACT-01_Flow2_20241115_HES-SO.xlsx", training=False)
-    clinical_data = clinical_data.assign(BMI=lambda x: round(x.WEIGHT / (x.HEIGHT / 100.0) ** 2, 3))
+    trial_data = pd.read_excel(
+        "input/PRE-ACT-01_Flow2_20241115_HES-SO.xlsx", index_col=False
+    ).iloc[:, :5]
+    clinical_data = dh.obtain_data(
+        "input/PRE-ACT-01_Flow2_20241115_HES-SO.xlsx", training=False
+    )
+    clinical_data = clinical_data.assign(
+        BMI=lambda x: round(x.WEIGHT / (x.HEIGHT / 100.0) ** 2, 3)
+    )
 
     p = Patient(trial_data, clinical_data)
 
@@ -29,9 +28,29 @@ def write_patient() -> Patient:
     clinical_data = clinical_data.assign(Lymphodema_NO=lambda x: 1)
     clinical_data = clinical_data.assign(Lymphodema_YES=lambda x: 0)
 
-    clinical_data.to_csv("input/test_sample_data.csv", header=False, index=False, sep=",")
+    clinical_data.to_csv(
+        "input/test_sample_data.csv", header=False, index=False, sep=","
+    )
 
     return p
+
+
+def write_samples_file(n: int) -> Patient:
+    patient_metadata = pd.read_excel(
+        "input/PRE-ACT-01_Flow2_20241115_HES-SO.xlsx", index_col=False
+    ).iloc[:, :5]
+    data = pd.read_csv("temp/train_data_normalized.csv", sep=" ", header=None)
+
+    sample_data = data.sample(n)
+
+    abspath = os.path.dirname(__file__)
+
+    patients = []
+    for i in range(n):
+        patient_metadata.iloc[0, 3] = f"FRA_TEST_00{i+1}" # FOR TESTING PURPOSES ONLY
+        patients.append(Patient(patient_metadata, sample_data.iloc[i, :], abspath))
+
+    return patients
 
 
 def write_attributes_file(attributes: list[str]) -> list[str]:
@@ -50,49 +69,6 @@ def write_train_data(data: pd.DataFrame, labels: pd.Series) -> None:
     data.to_csv("temp/train_data.csv", sep=",", header=False, index=False)
     labels.to_csv("temp/train_classes.csv", sep=",", header=False, index=False)
 
-
-def read_input(path: str) -> pd.DataFrame:
-    ext = os.path.splitext(path)[1].lower()
-
-    if ext in [".xls", ".xlsx", ".xlsm", ".xlsb", ".odf", ".ods", ".odt"]:
-        return pd.read_excel(path)
-    elif ext == ".csv":
-        return pd.read_csv(path)
-    else:
-        raise NotImplementedError(
-            f"Support for {ext} extension in {path} file is not implemented."
-        )
-
-
-def get_risk() -> float:
-    abspath = os.path.abspath(os.path.dirname(__file__))
-    preds_filepath = os.path.join(abspath, "input", "test_sample_pred.csv")
-
-    return np.loadtxt(preds_filepath)[1]
-
-
-def get_metrics() -> tuple[float, float]:
-    data = read_json_file(
-        os.path.join(
-            os.path.abspath(os.path.dirname(__file__)), "temp", "densClsMetrics.json"
-        )
-    )
-    nb_nets = data["nbNets"]
-
-    lower_conf_interval = (
-        data["avgs"][1] - (1.96 / math.sqrt(nb_nets)) * data["stds"][1]
-    )
-    upper_conf_interval = (
-        data["avgs"][1] + (1.96 / math.sqrt(nb_nets)) * data["stds"][1]
-    )
-
-    nb_decimals = 4
-    return (
-        round(lower_conf_interval, nb_decimals),
-        round(upper_conf_interval, nb_decimals),
-    )
-
-
 if __name__ == "__main__":
     args = init_args()
     abspath = os.path.abspath(os.path.dirname(__file__))
@@ -107,7 +83,6 @@ if __name__ == "__main__":
 
     data, labels = dh.obtain_data("dataset/clinical_complete_rev1.csv")
     data, labels = dh.filter_clinical(data, labels)
-
     # added BMI column
     data = data.assign(BMI=lambda x: round(x.WEIGHT / (x.HEIGHT / 100.0) ** 2, 3))
 
@@ -119,35 +94,28 @@ if __name__ == "__main__":
     update_config_files(abspath, nb_features, nb_classes)
     normalization("--json_config_file config/normalization.json")
 
-    patient = write_patient()
+    # patient = write_patient()
+    patients = write_samples_file(3)
 
     if args.train:
         dimlpBT("--json_config_file config/dimlpbt.json")
         fidexGloRules("--json_config_file config/fidexglorules.json")
 
-    densCls("--json_config_file config/denscls.json")
-    fidexGlo("--json_config_file config/fidexglo.json")
-    normalization("--json_config_file config/denormalization.json")
+    for patient in patients:
+        patient.extract_rules()
 
-    intervals = get_metrics()
-    risk = get_risk()
-    patient.set_metrics(risk, intervals[0], intervals[1])
+    # normalization("--json_config_file config/denormalization.json")
 
     # ! beware of normalized/denormalized data, denormalize explaination
-    selected_rules = read_json_file("temp/explanation.json")["samples"][0]
-    selected_rules = Rule.list_from_dict(selected_rules)
-    global_rules = GlobalRules.from_json_file("temp/global_rules.json")
+    # global_rules = GlobalRules.from_json_file("temp/global_rules.json")
+    # global_rules.set_rules_id()
 
-    global_rules.set_rules_id()
+    # for rule in selected_rules:
+    #     # TODO: comparison is broken
+    #     id = global_rules.get_rule_id(rule)
+    #     if id == -1:
+    #         id = global_rules.add_rule(rule)
+    #     rule.id = id
 
-    for rule in selected_rules:
-        # TODO: comparison is broken
-        id = global_rules.get_rule_id(rule)
-        if id == -1:
-            id = global_rules.add_rule(rule)
-        rule.id = id
-    # print(selected_rules)
-
-    patient.set_selected_rules(selected_rules)
-    print(patient.pretty_repr(attributes))
-    patient.write_results(attributes[:-2])
+    for patient in patients:
+        patient.write_results(attributes[:-2])
--- tests_ressources/src/patient.py
+++ tests_ressources/src/patient.py
@@ -1,34 +1,38 @@
 from __future__ import annotations
 import os
+import math
+import numpy as np
 import pandas as pd
 from src.rule import *
+from pathlib import Path
+from datetime import datetime
+from trainings import normalization
+from dimlpfidex.dimlp import densCls
+from dimlpfidex.fidex import fidexGlo
+from src.utils import update_config_file, read_json_file
 
 
 class Patient:
-    def __init__(self, patient_data: pd.DataFrame, clinical_data: pd.DataFrame) -> None:
+    def __init__(
+        self, patient_data: pd.DataFrame, clinical_data: pd.DataFrame, abspath: str
+    ) -> None:
         self.study_id = patient_data.iloc[0, 0]
         self.site_idn = patient_data.iloc[0, 1]
         self.site_name = patient_data.iloc[0, 2]
         self.subj_id = patient_data.iloc[0, 3]
         self.visit = patient_data.iloc[0, 4]
-        self.input_data = clinical_data
+        self.clinical_data = clinical_data
+
+        self.project_abspath = abspath
+        self.reldir = os.path.join("patients_data", self.subj_id)
+        self.absdir = os.path.join(abspath, self.reldir)
+        Path(self.absdir).mkdir(exist_ok=True)
 
         # properties to be set during rule extraction
         self.risk = -1
         self.low_interval = -1
         self.high_interval = -1
-
         self.selected_rules = []
-
-    def set_selected_rules(self, selected_rules: list[Rule]):
-        self.selected_rules = selected_rules
-
-    def set_metrics(
-        self, risk: float, low_interval: float, high_interval: float
-    ) -> None:
-        self.risk = risk
-        self.low_interval = low_interval
-        self.high_interval = high_interval
 
     def to_str_list(self) -> list[list[str]]:
         res = []
@@ -48,24 +52,91 @@ class Patient:
 
         return res
 
-    def prepare_input_for_extraction(self, path: str) -> str:
-        filepath = os.path.join(path, f"input_{self.id}.csv")
+    def __prepare_input_for_extraction(self) -> str:
+        input_filepath = os.path.join(self.absdir, "input_data.csv")
+        normalization_conf_file = os.path.join(
+            self.project_abspath, "config", "normalization.json"
+        )
 
         # one hotting classes (useless but must be done in order to work with densCls)
-        to_write = self.input_data.copy(deep=True).to_frame().T
+        to_write = self.clinical_data.to_frame().T.copy()
         to_write = to_write.assign(Lymphodema_NO=lambda x: 1)
         to_write = to_write.assign(Lymphodema_YES=lambda x: 0)
+        to_write.to_csv(input_filepath, sep=" ", header=False, index=False)
 
-        to_write.to_csv(filepath, sep=",", header=False, index=False)
+        update_config_file(
+            normalization_conf_file,
+            {
+                "data_files": f"[{self.reldir}/input_data.csv]",
+                "output_normalization_file": f"{self.reldir}/normalization_stats.txt",
+            },
+        )
+        # TODO: normalization("--json_config_file config/normalization.json") 
 
-        return filepath
+    def extract_rules(self):
+        denscls_conf_file = os.path.join(self.project_abspath, "config", "denscls.json")
+        fidexglo_conf_file = os.path.join(
+            self.project_abspath, "config", "fidexglo.json"
+        )
+
+        update_config_file(
+            denscls_conf_file,
+            {
+                "test_data_file": f"{self.reldir}/input_data.csv",
+                "test_pred_outfile": f"{self.reldir}/prediction.csv",
+                "metrics_file": f"{self.reldir}/densClsMetrics.json",
+                "console_file":  f"{self.reldir}/{datetime.today().strftime('%Y%m%d%H%M')}.log"
+            },
+        )
+
+        update_config_file(
+            fidexglo_conf_file,
+            {
+                "test_data_file": f"{self.reldir}/input_data.csv",
+                "test_pred_file": f"{self.reldir}/prediction.csv",
+                "explanation_file": f"{self.reldir}/extracted_rules.json",
+                "console_file":  f"{self.reldir}/{datetime.today().strftime('%Y%m%d%H%M')}.log"
+            },
+        )
+
+        self.__prepare_input_for_extraction()
+        densCls(f"--json_config_file {denscls_conf_file}")
+        fidexGlo(f"--json_config_file {fidexglo_conf_file}")
+
+        self.__set_metrics()
+        self.__set_risk()
+
+        selected_rules = read_json_file(f"{self.absdir}/extracted_rules.json")[
+            "samples"
+        ][0]
+        self.selected_rules = Rule.list_from_dict(selected_rules)
+
+        # normalization("--json_config_file config/denormalization.json")
+
+    def __set_metrics(self):
+        data = read_json_file(os.path.join(self.absdir, "densClsMetrics.json"))
+        nb_nets = data["nbNets"]
+
+        self.low_interval = (
+            data["avgs"][1] - (1.96 / math.sqrt(nb_nets)) * data["stds"][1]
+        )
+        self.high_interval = (
+            data["avgs"][1] + (1.96 / math.sqrt(nb_nets)) * data["stds"][1]
+        )
+
+    def __set_risk(self):
+        preds_filepath = os.path.join(self.absdir, "prediction.csv")
+        self.risk = np.loadtxt(preds_filepath)[1]
 
     def write_results(self, attributes: list[str]) -> None:
         unicancer_headers = ["VISIT", "STUDY_ID", "SITE_NAME", "SITENAME", "SUBJECT_ID"]
         rule_headers = ["RISK", "LOW_INTERVAL", "HIGH_INTERVAL", "RULE_ID"]
         headers = unicancer_headers + rule_headers + attributes
 
-        with open(f"output/results_{self.subj_id}.csv", "w") as fp:
+        with open(
+            f"{self.project_abspath}/output/{self.subj_id}_results_{datetime.today().strftime('%Y%m%d%H%M')}.csv",
+            "w",
+        ) as fp:
             fp.write(",".join(headers) + "\n")
             for row in self.to_str_list():
                 fp.write(",".join(row) + "\n")
@@ -85,7 +156,7 @@ Risk: {self.risk}
 Low conf. int.: {self.low_interval}
 High conf. int.: {self.high_interval}
 Patient's data: 
-{self.input_data}
+{self.clinical_data}
 Selected rules: 
 {rules_str}
         """
@@ -108,7 +179,7 @@ Risk: {self.risk}
 Low conf. int.: {self.low_interval}
 High conf. int.: {self.high_interval}
 Patient's data: 
-{self.input_data}
+{self.clinical_data}
 Selected rules: 
 {rules_str}
         """
