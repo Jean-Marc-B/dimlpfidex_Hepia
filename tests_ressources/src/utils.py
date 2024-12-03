ec0efa317eb3999aeff11f198205172bdfe26e69
Damian Boquete <damian.boquete@hotmail.com>
2024-12-03 13:25:26 +0100

    Refactored patients code, removed useless configs, removed useless code

--- tests_ressources/src/utils.py
+++ tests_ressources/src/utils.py
@@ -1,11 +1,9 @@
 import os
-import csv
 import json
 import argparse
-from pathlib import Path
 from datetime import datetime
 
-def get_most_recent_file(absolute_path: str) -> str:
+def get_most_recent_input_file(absolute_path: str) -> str:
     # get all filepaths inside input/ folder 
     list_filepaths = [os.path.join(absolute_path, filename) for filename in os.listdir(absolute_path)]
     # get most recent by comparing UNIX timestamps
@@ -60,46 +58,9 @@ def write_json_file(path: str, data: dict, mode: str = "w") -> None:
     with open(path, mode) as fp:
         json.dump(data, fp, indent=4)
 
-
-def write_csv(path: str, data: list[list]) -> None:
-    with open(path, "w") as fp:
-        wr = csv.writer(fp, quoting=csv.QUOTE_ALL)
-        wr.writerows(data)
-
-
-def check_working_subdirs(abspath: str) -> None:
-    dirs = ["temp", "logs"]
-
-    for dir in dirs:
-        path = os.path.join(abspath, dir)
-        Path(path).mkdir(exist_ok=True)
-
-
-def clean_dir(abspath: str, dirname: str):
-    nremoved = 0
-    temppath = os.path.join(
-        abspath,
-        dirname,
-    )
-
-    print(f"Cleaning {temppath} directory...")
-
-    for file in os.listdir(temppath):
-        filepath = os.path.join(temppath, file)
-        try:
-            if os.path.isfile(filepath):
-                os.remove(filepath)
-                nremoved += 1
-        except OSError as ose:
-            print(f"Error occured while trying to delete {file}. Error: {ose}")
-
-    print(f"{temppath} cleaned, {nremoved} files deleted.")
-
-
 def init_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--train", action="store_true")
-    parser.add_argument("-c", "--clean", action="store_true")
-    parser.add_argument("--cleanall", action="store_true")
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--test", type=int)
 
     return parser.parse_args()
