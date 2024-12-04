066ee634b6df5e9959393c008b9285a754b485ac
Damian Boquete <damian.boquete@hotmail.com>
2024-12-04 13:30:55 +0100

    Added installation script (to be tested)

---
+++ tests_ressources/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+git clone https://github.com/Jean-Marc-B/dimlpfidex_Hepia.git && \
+cd dimlpfidex_Hepia && \
+python -m venv .venv && \
+source .venv/bin/activate && \
+pip install . && \
+rm -rf bin build && \ mkdir build && \
+cd build && \
+cmake .. && \
+cmake --build . -j 8 && \
+cd .. && \
+python3 -m build && \
+pip uninstall dimlpfidex && \
+pip install dist/*.whl
