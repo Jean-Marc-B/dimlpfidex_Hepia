cde2fa5cb6354e0f63c2e1a7531f853f119975a1
Damian Boquete <damian.boquete@hotmail.com>
2025-04-16 15:52:53 +0200

    Added data vizualisation

--- preact/data_generator.py
+++ preact/data_generator.py
@@ -1,64 +1,78 @@
+import matplotlib
+from matplotlib import pyplot as plt
 from sys import argv
 import pandas as pd
 import random as r
+from math import ceil
+from os import makedirs
 
 
-def rand_float(column: pd.Series, delta: float, n:int) -> list[float]:
+def init_subplots(nrows:int, ncols:int, nplots:int) -> list:
+    subplots = [plt.subplots(nrows, ncols, figsize=(15,10)) for _ in range(nplots)] 
+
+    for i, subplot in enumerate(subplots):
+        subplot[0].suptitle(f"Data gen. comparision page {i+1}/{nplots}")
+        subplot[0].tight_layout()
+
+    return subplots
+
+
+def rand_number(column: pd.Series, n: int, precision: int = 0) -> list[float]:
+    # cleaned_col = column.dropna()
     mean = column.mean()
-    low_bound = mean - delta
-    up_bound = mean + delta
-    return [r.uniform(low_bound, up_bound) for _ in range(n)]
+    std = column.std()
+    res = [round(r.gauss(mean, std), precision) for _ in range(n)]
+
+    return [n if n >= 0.0 else 0.0 for n in res]
 
 
-def rand_int(column: pd.Series, delta: int, n: int) -> list[int]:
-    mean = round(column.mean(), 0)
-    low_bound = mean - delta
-    up_bound = mean + delta
-    return [r.randint(low_bound, up_bound) for _ in range(n)]
+def rand_choice(column: pd.Series, n: int) -> list[bool]:
+    cleaned_col = column.dropna()
+    size = cleaned_col.count()
+    values = cleaned_col.unique()
+    weights = (column.value_counts(sort=False) / size).to_list()
 
-
-def rand_choice(column: pd.Series, n: int) -> list:
-    return [r.choice(column.dropna().unique()) for _ in range(n)]
+    return r.choices(values, weights=weights, k=n)
 
 
 if __name__ == "__main__":
-    n = 10
+    n = 6000
     dataset_path = "dataset/clinical_complete_rev1.csv"
 
     if len(argv) == 2:
         nb_rows_to_gen = argv[1]
 
     data = pd.read_csv(dataset_path, index_col=False)
+    data = data.iloc[1:,]
 
-    generated_data = pd.DataFrame(
-        {
-    "height":                                   rand_int(data["height"], 20.0, n),
-    "weight":                                   rand_int(data["weight"], 20.0, n),
-    "Age":                                      rand_int(data["Age"], 40.0, n),
+    generated_data = pd.DataFrame({
+    "height":                                   rand_number(data["height"], n, 0),
+    "weight":                                   rand_number(data["weight"], n, 0),
+    "Age":                                      rand_number(data["Age"], n, 0),
     "Smoker":                                   rand_choice(data["Smoker"], n),
     "Menopausal":                               rand_choice(data["Menopausal"], n),
     "Diabetes":                                 rand_choice(data["Diabetes"], n),
     "Surgery":                                  rand_choice(data["Surgery"], n),
     "Type Surgery":                             rand_choice(data["Type Surgery"], n),
-    "Nodes involved":                           rand_float(data["Nodes involved"], 1.0, n),
+    "Nodes involved":                           rand_choice(data["Nodes involved"], n),
     "Side of Primary":                          rand_choice(data["Side of Primary"], n),
     "Histological Type":                        rand_choice(data["Histological Type"], n),
-    "Pathological tumour size":                 rand_int(data["Pathological tumour size"], 10.0, n),
+    "Pathological tumour size":                 rand_number(data["Pathological tumour size"], n, 0),
     "Clinical T-Stage":                         rand_choice(data["Clinical T-Stage"], n),
     "Clinical N-Stage" :                        rand_choice(data["Clinical N-Stage"], n),
-    "Ki-67 status":                             rand_int(data["Ki-67 status"], 20.0, n),
+    "Ki-67 status":                             rand_number(data["Ki-67 status"], n, 0),
     "ER Status":                                rand_choice(data["ER Status"], n),
     "HER-2 Status":                             rand_choice(data["HER-2 Status"], n),
     "PR Status":                                rand_choice(data["PR Status"], n),
     "Number of fractions":                      rand_choice(data["Number of fractions"], n),
-    "Mean Heart Dose":                          rand_float(data["Mean Heart Dose"], 2.5, n),
+    "Mean Heart Dose":                          rand_number(data["Mean Heart Dose"], n, 2),
     "IMRT":                                     rand_choice(data["IMRT"], n),
     "3D":                                       rand_choice(data["3D"], n),
     "Treated breast radio":                     rand_choice(data["Treated breast radio"], n),
     "Boost":                                    rand_choice(data["Boost"], n),
     "neoadjuvant chemotherapy":                 rand_choice(data["neoadjuvant chemotherapy"], n),
     "adjuvant chemotherapy":                    rand_choice(data["adjuvant chemotherapy"], n),
-    "Nodes examined":                           rand_int(data["Nodes examined"], 20.0, n),
+    "Nodes examined":                           rand_number(data["Nodes examined"], n, 0),
     "Baseline Arm Lymphedema":                  rand_choice(data["Baseline Arm Lymphedema"], n),
     "Post-RT Arm Lymphedema":                   rand_choice(data["Post-RT Arm Lymphedema"], n),
     "12m follow-up Arm Lymphedema":             rand_choice(data["12m follow-up Arm Lymphedema"], n),
@@ -110,8 +124,39 @@ if __name__ == "__main__":
     "Sentinel node biopsy":                     rand_choice(data["Sentinel node biopsy"], n),
     "Planned axillary dissection":              rand_choice(data["Planned axillary dissection"], n),
     "DB":                                       [0] * n,
-        }
-    )
-    generated_data = generated_data.assign(BMI=lambda x: round(x["weight"] / (x["height"] / 100.0) ** 2, 3))
-    print(generated_data.info())
-    print(generated_data)
+    })
+
+
+    nplots = 10
+    n_per_plot = ceil(len(generated_data.columns) / nplots)
+    ncols = 4
+    nrows = ceil(n_per_plot / ncols)
+
+    subplots = init_subplots(nrows, ncols, nplots)
+
+    n = 0
+    i_sp = 0
+    current_fig = subplots[i_sp][0]
+    current_subplot_axes = subplots[i_sp][1]
+
+    for name in generated_data:
+        if n == n_per_plot:
+            current_fig.savefig(f"plots/subplot_{i_sp+1}of{nplots}.png")
+            i_sp +=1
+            n = 0
+            current_subplot_axes = subplots[i_sp][1]
+            current_fig = subplots[i_sp][0]
+
+        ir = n // ncols
+        ic = n % ncols
+
+        original_col = data[name].dropna()
+        generated_col = generated_data[name].dropna()
+
+        current_subplot_axes[ir, ic].set_title(name, fontsize=10)
+        current_subplot_axes[ir, ic].hist(original_col, alpha=0.5)
+        current_subplot_axes[ir, ic].hist(generated_col, alpha=0.5)
+        n+=1
+
+    makedirs("plots",exist_ok=True)
+    current_fig.savefig(f"plots/subplot_{nplots}of{nplots}.png") 
