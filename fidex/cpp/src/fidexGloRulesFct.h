e313581fa47d34ff6cc20847646a125c9a7ebcf5
jeanmarc.boutay <jeanmarc.boutay@isc-phenix.hes.adhes.hesge.ch>
2026-03-06 15:41:55 +0100

    Corrected bugs and optimized fidexGloRules

--- fidex/cpp/src/fidexGloRulesFct.h
+++ fidex/cpp/src/fidexGloRulesFct.h
@@ -1,29 +1,12 @@
 #ifndef FIDEXGLORULESFCT_H
 #define FIDEXGLORULESFCT_H
 
-#include "../../../common/cpp/src/checkFun.h"
-#include "../../../common/cpp/src/dataSet.h"
-#include "../../../common/cpp/src/errorHandler.h"
-#include "../../../common/cpp/src/parameters.h"
-#include "../../../common/cpp/src/rule.h"
-#include "fidexAlgo.h"
-#include "hyperLocus.h"
-#include <algorithm>
-#include <chrono>
-#include <limits.h>
-#include <numeric>
-#include <omp.h>
-#include <random>
-#include <sstream>
 #include <string>
-#include <time.h>
-#include <sys/stat.h>
+#include <vector>
 
-#ifdef _WIN32
-#include "../../../dirent/include/dirent.h"
-#else
-#include <dirent.h>
-#endif
+class DataSetFid;
+class Parameters;
+class Rule;
 
 /**
  * @brief Displays the parameters for fidexGloRules.
